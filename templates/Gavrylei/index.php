d9646896a16276f5bc6931b94a1066878132f0f8
Alyona <alyona@alyona-Lenovo-B560.(none)>
2013-11-04 00:03:10 +0200

    edit css,add articles

--- templates/Gavrylei/index.php
+++ templates/Gavrylei/index.php
@@ -5,15 +5,16 @@ defined('_JEXEC') or die ( 'Restricted access' );?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
+
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
     <title>GeekHub - отримай практичні знання в сфері IT</title>
 
     <jdoc:include type="head" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ;?>/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ;?>/css/flipclock.css" />
 
 </head>
 
